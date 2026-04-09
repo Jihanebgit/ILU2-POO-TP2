2f8b9fab1159d87b82ed1c2079644ae8ee36abd3
Jihanebgit <jihanebattas025@gmail.com>
2026-04-09 18:00:37 +0200

    testPrendreEtal modifiee

--- test/controleur/ControlPrendreEtalTest.java
+++ test/controleur/ControlPrendreEtalTest.java
@@ -32,6 +32,19 @@ class ControlPrendreEtalTest {
 	@Test
 	void testResteEtals() {
 		ControlPrendreEtal controlPrendreEtal=new ControlPrendreEtal(ControlVerifierIdentite, village);
+		village.installerVendeur(abraracourcix, "fleurs", 3);
+		assertTrue(controlPrendreEtal.resteEtals());
+		Gaulois bonemine= new Gaulois("Bonemine", 2);
+		Gaulois racour= new Gaulois("racour",3);
+		Gaulois ludo= new Gaulois("Ludo", 2);
+		Gaulois fanny= new Gaulois("Fanny",3);
+		village.installerVendeur(bonemine, "panier", 3);
+		assertTrue(controlPrendreEtal.resteEtals());
+		village.installerVendeur(racour, "carton", 3);
+		village.installerVendeur(ludo, "nourriture", 3);
+		village.installerVendeur(fanny, "cahier", 3);
+		assertFalse(controlPrendreEtal.resteEtals());
+		
 	}
 
 	@Test
