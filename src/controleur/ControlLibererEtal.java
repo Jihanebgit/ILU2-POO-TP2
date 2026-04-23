b3ad897765f6160c44ab5dbd67696e6f1994576e
Jihanebgit <jihanebattas025@gmail.com>
2026-04-23 18:45:33 +0200

    suite 1

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -5,42 +5,31 @@ import villagegaulois.Etal;
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
-	public ControlLibererEtal(
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur) {
+	public ControlLibererEtal(ControlTrouverEtalVendeur controlTrouverEtalVendeur) {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
-
 
 	/**
 	 * 
 	 * @param nomVendeur
-	 * @return donneesEtal est un tableau de chaine contenant
-	 * 		[0] : un boolean indiquant si l'étal est occupé
-	 * 		[1] : nom du vendeur
-	 * 		[2] : produit vendu
-	 * 		[3] : quantité de produit à vendre au début du marché
-	 * 		[4] : quantité de produit vendu
+	 * @return donneesEtal est un tableau de chaine contenant [0] : un boolean
+	 *         indiquant si l'étal est occupé [1] : nom du vendeur [2] : produit
+	 *         vendu [3] : quantité de produit à vendre au début du marché [4] :
+	 *         quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		String[] donneesEtal= new String[5];
 		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
-		Boolean etalOccupe=etal.isEtalOccupe();
-		String[] donneesVentes=etal.etatEtal();
-		String produit= donneesVentes[2];
-		int quantiteInitial=Integer.parseInt(donneesVentes[3]);
-		int quantiteVendu=Integer.parseInt(donneesVentes[4]);
-		
-		donneesEtal[0]=etalOccupe.toString();	//boolean en string de vide ou non 
-		donneesEtal[1]=nomVendeur;	// nom du vendeur 
-		donneesEtal[2]=produit;	// nom du produit 
-		donneesEtal[3]=Integer.toString(quantiteInitial);	// quantite au debut 
-		donneesEtal[4]=Integer.toString(quantiteVendu);	// quantite vendu après
-		return donneesEtal; //un tableau avec les donnees de l'etal 
+		if (etal == null) {
+			String[] donneesEtal = { "null" };
+			return donneesEtal;
+		}
+		String[] etat=etal.etatEtal();
+		etal.libererEtal();
+		return etat;
 	}
 
 	public boolean isVendeur(String nomVendeur) {
-		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
-		return (etal!= null);
+		return controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur) != null;
 	}
 
 }
--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -10,20 +10,19 @@ public class BoundaryLibererEtal {
 	}
 
 	public void libererEtal(String nomVendeur) {
-		boolean vendeurReconnu = controlLibererEtal.isVendeur(nomVendeur);
-		if (!vendeurReconnu) {
-			System.out.println("Mais vous n'êtes pas inscrit sur notre " + "marché aujourd'hui !");
-
-		} else {
-			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
-			boolean etalOccupe = Boolean.parseBoolean(donneesEtal[0]);
-			String produit = donneesEtal[2];
-			int quantiteInitial = Integer.parseInt(donneesEtal[3]);
-			int quantiteVendu = Integer.parseInt(donneesEtal[4]);
-			if (etalOccupe) {
-				System.out
-						.println("Vous avez vendu " + quantiteVendu + " sur " + quantiteInitial + " " + produit + ".");
-				System.out.println("Au revoir " + nomVendeur + ", passez une bonne journée");
+		if (!controlLibererEtal.isVendeur(nomVendeur)) {
+			System.out.println("Mais vous n'êtes pas inscrit sur notre marché aujourd'hui !");
+		}else {
+			String[] donneesEtal=controlLibererEtal.libererEtal(nomVendeur);
+			if(donneesEtal[0].equals("true")) {
+				StringBuilder chaine = new StringBuilder("Vous avez vendu ");
+				chaine.append(donneesEtal[4]);
+				chaine.append(" sur ");
+				chaine.append(donneesEtal[3]);
+				chaine.append(" ");
+				chaine.append(donneesEtal[2]);
+				chaine.append(".");
+				System.out.println(chaine.toString());
 			}
 		}
 	}
